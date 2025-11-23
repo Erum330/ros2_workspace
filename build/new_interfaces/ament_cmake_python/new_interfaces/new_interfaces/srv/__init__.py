e919c4d5e012366170a140f27bb1279d5df77b5c
Erum330 <58556663+Erum330@users.noreply.github.com>
2025-11-23 19:58:09 +0400

    SLAM done

--- build/new_interfaces/ament_cmake_python/new_interfaces/new_interfaces/srv/__init__.py
+++
@@ -1,8 +0,0 @@
-from new_interfaces.srv._add_two_ints import AddTwoInts  # noqa: F401
-from new_interfaces.srv._add_two_ints import AddTwoInts_Event  # noqa: F401
-from new_interfaces.srv._add_two_ints import AddTwoInts_Request  # noqa: F401
-from new_interfaces.srv._add_two_ints import AddTwoInts_Response  # noqa: F401
-from new_interfaces.srv._rectangle_area import RectangleArea  # noqa: F401
-from new_interfaces.srv._rectangle_area import RectangleArea_Event  # noqa: F401
-from new_interfaces.srv._rectangle_area import RectangleArea_Request  # noqa: F401
-from new_interfaces.srv._rectangle_area import RectangleArea_Response  # noqa: F401
